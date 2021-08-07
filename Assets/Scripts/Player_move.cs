1df475e0b55bec449eda0b35a7d81be0ae4101df
blindfolded32 <sunsya@yandex.ru>
2021-08-07 21:12:58 +0300

    Movement added

--- Assets/Scripts/Player_move.cs
+++ Assets/Scripts/Player_move.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class Player_move : MonoBehaviour
+namespace Scripts.Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Player_move : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
         
+        private CharacterController _MovementControl;
+        private Vector3 _MoveDirection = Vector3.zero;
+        public float MoveSpeed, RotateSpeed;
+        private Animator _animator;
+
+        /*   private CharacterController controller;
+           private Vector3 playerVelocity;
+           private bool groundedPlayer;
+           private float playerSpeed = 2.0f;
+           private float jumpHeight = 1.0f;
+           private float gravityValue = -9.81f;
+
+           private void Start()
+           {
+               controller = gameObject.GetComponent<CharacterController>();
+           }
+
+           void Update()
+           {
+
+
+               Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+               controller.Move(move * Time.deltaTime * playerSpeed);
+
+               if (move != Vector3.zero)
+               {
+                   gameObject.transform.forward = move;
+               }
+               controller.Move(playerVelocity * Time.deltaTime);
+           }*/
+        private void Start()
+        {
+            _MovementControl = GetComponent<CharacterController>();
+        }
+        void Update()
+        {
+            Movement();
+        }
+
+
+        private void Movement()
+        {
+         
+            
+              
+                _MoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                _MoveDirection = transform.TransformDirection(_MoveDirection);
+                _MoveDirection *= MoveSpeed;
+
+            _MovementControl.Move(_MoveDirection * Time.deltaTime);
+            transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+
+        }
+
     }
 }
