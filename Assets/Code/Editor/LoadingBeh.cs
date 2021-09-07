e2f4887e3f8394132919d57dc401af39c71934e5
blindfolded32 <sunsya@yandex.ru>
2021-09-07 14:56:28 +0300

    Editor Items

---
+++ Assets/Code/Editor/LoadingBeh.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Code.Editor
+{
+    public class LoadingBeh
+    {
+        public sealed class TestEditorBehaviour : MonoBehaviour
+        {
+            public float Count = 42;
+            public int Step = 2;
+            private void Start()
+            {
+#if UNITY_EDITOR
+                for (var i = 0; i < Count; i++)
+                {
+                    EditorUtility.DisplayProgressBar("Loading", $" % {i}",
+                        i / Count);
+                    Thread.Sleep(Step * 100);
+                }
+                EditorUtility.ClearProgressBar();
+                var isPressed = EditorUtility.DisplayDialog("Choose your desteny", @"What pill? ", "Red", "Blue");
+                if (isPressed)
+                {
+                    EditorApplication.isPaused = true;
+                }
+#endif
+            }
+        }
+    }
+}
---
+++ Assets/Code/Editor/ObectAddEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using  UnityEngine;
+namespace Code.Editor
+{
+    [CustomEditor(typeof(EditorBeh))]
+    public class ObectAddEditor : UnityEditor.Editor
+    {
+        private bool _isPressButtonOk;
+
+        public override void OnInspectorGUI()
+        {
+            EditorBeh testTarget = (EditorBeh) target;
+
+            testTarget.count = EditorGUILayout.IntSlider(testTarget.count, 10, 50);
+            testTarget.offset = EditorGUILayout.IntSlider(testTarget.offset, 1, 5);
+
+            testTarget.obj =
+                EditorGUILayout.ObjectField("Объект который хотим вставить",
+                        testTarget.obj, typeof(GameObject), false)
+                    as GameObject;
+
+            var isPressButton = GUILayout.Button("Создание объектов по кнопке",
+                EditorStyles.miniButtonLeft);
+
+            _isPressButtonOk = GUILayout.Toggle(_isPressButtonOk, "Ok");
+
+            if (isPressButton)
+            {
+                testTarget.CreateObj();
+                _isPressButtonOk = true;
+            }
+
+            if (_isPressButtonOk)
+            {
+                testTarget.Test = EditorGUILayout.Slider(testTarget.Test, 10, 50);
+                EditorGUILayout.HelpBox("Вы нажали на кнопку", MessageType.Warning);
+
+                var isPressAddButton = GUILayout.Button("Add Component",
+                    EditorStyles.miniButtonLeft);
+                var isPressRemoveButton = GUILayout.Button("Remove Component",
+                    EditorStyles.miniButtonLeft);
+                if (isPressAddButton)
+                {
+                    testTarget.AddComponent();
+                }
+
+                if (isPressRemoveButton)
+                {
+                    testTarget.RemoveComponent();
+                }
+            }
+        }
+    }
+}
